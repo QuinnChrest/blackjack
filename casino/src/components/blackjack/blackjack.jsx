382a12b2a151a05cea7b3b2f7bf9f872a59d19e9
Quinn Chrest <quinnchr@gmail.com>
2024-05-02 18:17:13 -0500

    add a gamestate and clean up some code.

--- casino/src/components/blackjack/blackjack.jsx
+++ casino/src/components/blackjack/blackjack.jsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Deck } from "../../models/deck";
 import Card from "../card/card";
 import "./blackjack.css";
 
+const GameState = Object.freeze({
+  Start: 0,
+  Dealt: 1,
+  Bust: 2,
+  Stand: 3,
+});
+
 function Blackjack() {
-  const [total, setTotal] = useState(50);
-  const [bet, setBet] = useState(0);
+  const [total, setTotal] = useState(1000);
+  const [bet, setBet] = useState(10);
   const [playerHand, setPlayerHand] = useState([]);
   const [dealerHand, setDealerHand] = useState([]);
-  const [betInput, setBetInput] = useState(0);
-  const [endTurn, setEndTurn] = useState(false);
+  const [state, setState] = useState(GameState.Start);
 
-  const [disableDD, setDisableDD] = useState(false);
   const [disableSplit, setDisableSplit] = useState(true);
 
   var deck = Deck.GetBlackJackDeck(6);
+
+  useEffect(() => CheckForBust(playerHand), [playerHand]);
 
   function Deal() {
     deck.pop();
@@ -31,26 +38,49 @@ function Blackjack() {
     setDealerHand(tempDealerHand);
 
     setDisableSplit(tempPlayerHand[0].value != tempPlayerHand[1].value);
-    setDisableDD(bet * 2 > total);
+
+    setState(GameState.Dealt);
 
     setTotal(total - bet);
   }
 
-  function Bet() {
-    if (betInput >= 0 && betInput <= total) {
-      setBet(betInput);
+  function Bet(amount) {
+    if (amount == 0) {
+      setBet(0);
     } else {
-      alert("invalid bet");
+      setBet(bet + amount);
     }
   }
 
   function Hit() {
-    let tempPlayerHand = [...playerHand, deck.pop()];
+    setPlayerHand([...playerHand, deck.pop()]);
+  }
 
-    setPlayerHand(tempPlayerHand);
+  function DoubleDown() {
+    setTotal(total - bet);
+    setBet(bet * 2);
+    setPlayerHand([...playerHand, deck.pop()]);
+  }
 
+  function Split() {
+    console.log("Split");
+  }
+
+  function Stand() {}
+
+  function Bust() {
+    setState(GameState.Bust);
+  }
+
+  function Reset() {
+    setPlayerHand([]);
+    setDealerHand([]);
+    setState(GameState.Start);
+  }
+
+  function CheckForBust(hand) {
     let handTotal = 0;
-    for (const card of tempPlayerHand) {
+    for (const card of hand) {
       if (isNaN(card.value)) {
         if (card.value == "A") {
           handTotal += 1;
@@ -63,39 +93,15 @@ function Blackjack() {
     }
 
     if (handTotal > 21) {
-      setTimeout(() => {
-        Bust();
-      }, 2000);
+      Bust();
     }
-  }
-
-  function DoubleDown() {
-    setBet;
-  }
-
-  function Split() {
-    console.log("Split");
-  }
-
-  function Stand() {
-    setEndTurn(true);
-  }
-
-  function Bust() {
-    setBet(0);
-    Reset();
-  }
-
-  function Reset() {
-    setPlayerHand([]);
-    setDealerHand([]);
   }
 
   return (
     <>
       <div className="Hand d-flex">
         {dealerHand.map((card, index) => (
-          <Card card={card} facedown={index == 0 && !endTurn} />
+          <Card card={card} facedown={index == 0 && state == GameState.Dealt} />
         ))}
       </div>
 
@@ -112,22 +118,57 @@ function Blackjack() {
         <span className="ms-1">{bet}</span>
       </div>
 
-      {playerHand.length == 0 ? (
-        <div className="menu">
-          <button type="button" className="btn btn-primary" onClick={() => Deal()}>
-            Deal
-          </button>
-          <input type="number" value={betInput} onChange={(event) => setBetInput(event.target.value)} />
-          <button type="button" className="btn btn-primary" onClick={() => Bet()}>
-            Bet
-          </button>
+      {state == GameState.Start && (
+        <div>
+          <div>
+            <button type="button" className="btn btn-primary" disabled={bet == 0} onClick={() => Deal()}>
+              Deal
+            </button>
+          </div>
+          <div>
+            <button type="button" className="btn btn-primary" disabled={bet + 50 > total} onClick={() => Bet(50)}>
+              +50
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet + 25 > total} onClick={() => Bet(25)}>
+              +25
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet + 10 > total} onClick={() => Bet(10)}>
+              +10
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet + 5 > total} onClick={() => Bet(5)}>
+              +5
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet + 1 > total} onClick={() => Bet(1)}>
+              +1
+            </button>
+            <button type="button" className="btn btn-primary" onClick={() => Bet(0)}>
+              0
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet - 1 < 0} onClick={() => Bet(-1)}>
+              -1
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet - 5 < 0} onClick={() => Bet(-5)}>
+              -5
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet - 10 < 0} onClick={() => Bet(-10)}>
+              -10
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet - 25 < 0} onClick={() => Bet(-25)}>
+              -25
+            </button>
+            <button type="button" className="btn btn-primary" disabled={bet - 50 < 0} onClick={() => Bet(-50)}>
+              -50
+            </button>
+          </div>
         </div>
-      ) : (
+      )}
+
+      {state == GameState.Dealt && (
         <div className="menu">
           <button type="button" className="btn btn-primary" onClick={() => Hit()}>
             Hit
           </button>
-          <button type="button" className="btn btn-primary" disabled={disableDD} onClick={() => DoubleDown()}>
+          <button type="button" className="btn btn-primary" disabled={bet > total || playerHand.length > 2} onClick={() => DoubleDown()}>
             Double Down
           </button>
           <button type="button" className="btn btn-primary" disabled={disableSplit} onClick={() => Split()}>
@@ -135,6 +176,14 @@ function Blackjack() {
           </button>
           <button type="button" className="btn btn-primary" onClick={() => Stand()}>
             Stand
+          </button>
+        </div>
+      )}
+
+      {state == GameState.Bust && (
+        <div className="menu">
+          <button type="button" className="btn btn-primary" onClick={() => Reset()}>
+            Reset
           </button>
         </div>
       )}
